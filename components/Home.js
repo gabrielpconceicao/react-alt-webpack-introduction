8e72bac73bd374822130caebff50acfa397a22ea
Gabriel Conceição <gabriel.conceicao@findhit.com>
2015-11-23 11:52:08 +0000

    Fix stores

--- components/Home.js
+++ components/Home.js
@@ -4,6 +4,12 @@ import SearchRepos from '../actions/SearchRepos'
 import Repo from '../stores/Repo'
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super( props )
+    this.state = { repo: {
+      total_count: undefined,
+    } }
+  }
 
   static getStores() {
 	  return [Repo];
@@ -15,19 +21,24 @@ export default class Home extends React.Component {
 
   render() {
     return (
-    	<div>Hello {this.props.repo}
+    	<div>Found {this.state.repo.total_count } Repos
     		<button onClick={this.handleClick}>Find repos</button>
-    	}
     	</div>
     	);
+  }
+
+  shouldComponentUpdate ( nextProps, nextState ) {
+    return true
   }
 
   componentWillMount() {
     Repo.listen(this.onChange.bind( this ) )
   }
+
   componentWillUnmount() {
     Repo.unlisten(this.onChange.bind( this ) )
   }
+
   onChange() {
     this.setState(Repo.getState())
   }
